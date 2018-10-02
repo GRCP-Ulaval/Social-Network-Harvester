5de3b349e1c72fc7872077205286f8fa8cba9bd9
gh-scloutier <scloutier@nexapp.ca>
2018-10-02 13:34:01 -0400

    Added migrations

--- SocialNetworkHarvester/Facebook/models/__init__.py
+++ SocialNetworkHarvester/Facebook/models/__init__.py
@@ -1,9 +1,20 @@
 from .FBAttachment import FBAttachment
 from .FBComment import FBComment
-from .FBPost import FBPost
-from .FBReaction import FBReaction
-from .FBProfile import FBProfile
-from .FBUser import FBUser
 from .FBLocation import FBLocation
-from .FBVideo import FBVideo
 from .FBPage import FBPage
+from .FBPost import FBPost
+from .FBProfile import FBProfile
+from .FBReaction import FBReaction
+from .FBTimeLabel import (
+    like_count,
+    comment_count,
+    share_count,
+    checkins_count,
+    fan_count,
+    overall_star_rating_count,
+    rating_count,
+    talking_about_count,
+    were_here_count
+)
+from .FBUser import FBUser
+from .FBVideo import FBVideo
